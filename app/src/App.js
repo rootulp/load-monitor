d9cc578b2fc8233f44ad67e19966f444c4f0b906
Rootul Patel <rootulp@palantir.com>
2019-01-12 18:17:30 -0800

    Fetch data from app -> server

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        data: {}
+      };
+  }
+
+  componentDidMount() {
+    fetch(`/data`).then(response => response.json()).then(
+      json => {
+        console.log(json);
+        this.setState({data: json});
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,14 +27,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>
+            {JSON.stringify(this.state.data)}
+          </p>
         </header>
       </div>
     );
