0ae3cbfbd6407fe2c642c07ff1c397501d10c77f
Rootul Patel <rootulp@palantir.com>
2019-01-12 19:11:38 -0800

    More chart improvements

--- app/src/App.js
+++ app/src/App.js
@@ -22,15 +22,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h6>History of load</h6>
         <LineChart width={800} height={800} data={this.state.data['data']} margin={{ top: 50, bottom: 50, left:50, right:50}}>
           <XAxis dataKey="time"/>
           <YAxis/>
           <Tooltip/>
           <CartesianGrid strokeDasharray="3 3"/>
           <Legend />
-          <Line type="monotone" dataKey="one_min" stroke="#8884d8" />
-          <Line type="monotone" dataKey="five_min" stroke="#82ca9d" />
-          <Line type="monotone" dataKey="fifteen_min" stroke="#8884d8" />
+          <Line type="monotone" dataKey="one_min" name="1 min avg" stroke="#8884d8" />
+          <Line type="monotone" dataKey="five_min" name="5 min avg" stroke="#82ca9d" />
         </LineChart>
       </div>
     );
