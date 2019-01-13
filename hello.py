d3a24e4ce499738ab632cda4edf1f4ef21dc3196
Rootul Patel <rootulp@palantir.com>
2019-01-12 17:20:29 -0800

    Save load/time and serve via flask

---
+++ hello.py
@@ -0,0 +1,27 @@
+import os
+import threading
+
+from datetime import datetime
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+store = {}
+store['data'] = {}
+
+
+@app.route('/data')
+def data():
+    global store
+    return jsonify(store)
+
+def save_load_every_minute():
+    global store
+    threading.Timer(5.0, save_load_every_minute).start()
+
+    time = datetime.now().isoformat()
+    load = os.getloadavg()
+    app.logger.debug('Saving load {} at {}'.format(load, time))
+    store['data'][time] = load
+
+save_load_every_minute()
