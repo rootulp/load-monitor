c759fab5189863a42020280c4676610c203edadd
Rootul Patel <rootulp@palantir.com>
2019-01-12 19:21:10 -0800

    Make chart responsive

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { CartesianGrid, LineChart, Line, Tooltip, Legend, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, LineChart, Line, Tooltip, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 class App extends Component {
   constructor(props) {
@@ -22,8 +22,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h6>Key statistics</h6>
+
+
       <h6>History of load</h6>
-        <LineChart width={800} height={800} data={this.filterToLastTenMins(this.state.data['data'])} margin={{ top: 50, bottom: 50, left:50, right:50}}>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart width={800} height={400} data={this.filterToLastTenMins(this.state.data['data'])} margin={{ top: 50, bottom: 50, left:50, right:50}}>
           <XAxis dataKey="time"/>
           <YAxis/>
           <Tooltip/>
@@ -32,6 +36,8 @@ class App extends Component {
           <Line type="monotone" dataKey="one_min" name="1 min avg" stroke="#8884d8" />
           <Line type="monotone" dataKey="five_min" name="5 min avg" stroke="#82ca9d" />
         </LineChart>
+      </ResponsiveContainer>
+
       </div>
     );
   }
