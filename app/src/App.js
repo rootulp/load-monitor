be7730dce0625992167a263e162165694c8ec252
Rootul Patel <rootulp@palantir.com>
2019-01-12 19:28:09 -0800

    Use moment to improve x-axis labels

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CartesianGrid, LineChart, Line, Tooltip, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import moment from 'moment'
 
 class App extends Component {
   constructor(props) {
@@ -28,7 +29,10 @@ class App extends Component {
       <h6>History of load</h6>
       <ResponsiveContainer width="100%" height={400}>
         <LineChart width={800} height={400} data={this.filterToLastTenMins(this.state.data['data'])} margin={{ top: 50, bottom: 50, left:50, right:50}}>
-          <XAxis dataKey="time"/>
+          <XAxis
+            dataKey="time"
+            tickFormatter = {(isoTime) => moment(isoTime).fromNow()}
+          />
           <YAxis/>
           <Tooltip/>
           <CartesianGrid strokeDasharray="3 3"/>
