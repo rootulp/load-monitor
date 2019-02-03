e6fdb50330788cf3c5f7c90d3bbbf6e90e5de093
Rootul Patel <rootulp@palantir.com>
2019-02-02 18:47:07 -0800

    Remove key statistics

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        currentTime: '',
         loadHistory: []
       };
   }
@@ -23,38 +22,27 @@ class App extends Component {
   }
 
   render() {
-    const { loadHistory, currentTime} = this.state;
+    const { loadHistory } = this.state;
 
     return (
       <div className="App">
-      <h6>Key statistics</h6>
-      {this.maybeRenderCurrentTime()}
-
-      <h6>Load history</h6>
-      <ResponsiveContainer width="100%" height={400}>
-        <LineChart width={800} height={400} data={this.filterToLastTenMins(loadHistory)} margin={{ top: 50, bottom: 50, left:50, right:50}}>
-          <XAxis
-            dataKey="time"
-            tickFormatter = {(isoTime) => moment(isoTime).fromNow()}
-          />
-          <YAxis/>
-          <Tooltip/>
-          <CartesianGrid strokeDasharray="3 3"/>
-          <Legend />
-          <Line type="monotone" dataKey="one_min" name="1 min avg" stroke="#8884d8" />
-          <Line type="monotone" dataKey="five_min" name="5 min avg" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-
+        <h1>Load history</h1>
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart width={800} height={400} data={this.filterToLastTenMins(loadHistory)} margin={{ top: 50, bottom: 50, left:50, right:50}}>
+            <XAxis
+              dataKey="time"
+              tickFormatter = {(isoTime) => moment(isoTime).fromNow()}
+            />
+            <YAxis/>
+            <Tooltip/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Legend />
+            <Line type="monotone" dataKey="one_min" name="1 min avg" stroke="#8884d8" />
+            <Line type="monotone" dataKey="five_min" name="5 min avg" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     );
-  }
-
-  maybeRenderCurrentTime() {
-    const {currentTime} = this.state
-    if (currentTime !== '') {
-      return (<ul><li>Current time: {moment(currentTime).toDate()}</li></ul>)
-    }
   }
 
   filterToLastTenMins(loadHistory) {
